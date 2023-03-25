2bb0eed52adeb0c0d857be9b123762086e433b29
pan_ziang <2385863274@qq.com>
2023-03-25 15:24:39 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <m-header></m-header>
+  <tab></tab>
+  <router-view :style="viewStyle" v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component"/>
+    </keep-alive>
+  </router-view>
+  <router-view
+    :style="viewStyle"
+    name="user"
+    v-slot="{ Component }"
+  >
+    <transition appear name="slide">
+      <keep-alive>
+        <component :is="Component"/>
+      </keep-alive>
+    </transition>
+  </router-view>
+  <player></player>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from '@/components/header/header'
+  import Tab from '@/components/tab/tab'
+  import Player from '@/components/player/player'
+  import { mapState } from 'vuex'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      Player,
+      MHeader: Header,
+      Tab
+    },
+    computed: {
+      viewStyle() {
+        const bottom = this.playlist.length ? '60px' : '0'
+        return {
+          bottom
+        }
+      },
+      ...mapState([
+        'playlist'
+      ])
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
